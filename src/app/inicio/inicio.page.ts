95a064c0be6c75597078d4c608e453aab1c16a50
LeonardoA <larroyos1@toluca.tecnm.mx>
2020-05-21 18:51:53 -0500

    Distancia entre dos coordenadas
    En esta versión se permite fijar la distancia en la que nos ubicamos, así como calcular la distancia entre las coordenadas fijas y las actuales, muestra en caso de encontrase a una distancia de dos metros un estado de puerta abierta y cerrada

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -12,38 +12,47 @@ export class Inicio {
 
   lblNotificaciones = 'NOTIFICACIONES';
 
-  EstadoGPS = ['ACTIVO','DESACTIVO'] as const;
+  EstadoGPS = ['ACTIVO', 'DESACTIVO'] as const;
 
-  options = {maximumAge: 0, timeout: 10000, enableHighAccuracy:true} as const;
+  opciones = { maximumAge: 0, timeout: 10000, enableHighAccuracy: true } as const;
 
   lblEstadoGPS: string;
 
   logs: String[] = [];
 
-  constructor(private diagnostico: Diagnostic,private geolocation: Geolocation) {
+  Distancia = 0.0000;
+  Puerta = 'Cerrada';
+
+  PosicionFija = { 'latitud': 0, 'longitud': 0 };
+  PosicionActual = { 'latitud': 1, 'longitud': 1 };
+
+  constructor(private diagnostic: Diagnostic, private geolocation: Geolocation) {
 
     this.lblEstadoGPS = this.EstadoGPS[1];
 
   }
 
-  FuncionaGPS = (resultado) => {
+  FuncionaLocalizacion = (resultado) => {
 
-    try{
-      
-      this.logs.push("latitud : "+resultado.coords.latitude+" longitud : "+resultado.coords.longitude);
-      
-      if(this.lblEstadoGPS!=this.EstadoGPS[0]){
+    try {
+
+      this.PosicionActual['latitud'] = resultado.coords.latitude;
+      this.PosicionActual['longitud'] = resultado.coords.longitude;
+
+      this.distanciaCoord();
+
+      if (this.lblEstadoGPS != this.EstadoGPS[0]) {
 
         this.lblEstadoGPS = this.EstadoGPS[0];
 
       }
-      
+
 
     }
-    catch(error){ 
+    catch (error) {
 
       //var positionError = (resultado as PositionError);
-      
+
       //this.logs.push('Permiso'+positionError.PERMISSION_DENIED);
 
       this.lblEstadoGPS = this.EstadoGPS[1];
@@ -53,43 +62,95 @@ export class Inicio {
 
   }
 
-  ErrorGPS = (positionError) => {
+  ErrorLocalizacion = (positionError) => {
 
-    this.logs.push('Permiso'+positionError.PERMISSION_DENIED);
+    this.logs.push('Denegado ' + positionError.PERMISSION_DENIED +
+      ' Desabilitado ' + positionError.POSITION_UNAVAILABLE +
+      ' Fuera de tiempo ' + positionError.TIMEOUT);
 
-    if(positionError.PERMISSION_DENIED==0){
+    //if(positionError.PERMISSION_DENIED==0){
 
-      this.lblEstadoGPS = this.EstadoGPS[1];
+    this.lblEstadoGPS = this.EstadoGPS[1];
+
+    //}
+
+  }
+
+  Localizacion() {
+
+    //navigator.geolocation.getCurrentPosition(this.FuncionaGPS,this.ErrorGPS,this.opciones);
+    navigator.geolocation.watchPosition(this.FuncionaLocalizacion, this.ErrorLocalizacion, this.opciones);
+
+  }
+
+  FuncionaGPS = (habilitado) => {
+
+    this.logs.push("GPS location is " + (habilitado ? "enabled" : "disabled"));
+
+  }
+
+  ErrorGPS = (error) => {
+
+    this.logs.push("The following error occurred: ");
+
+  }
+
+  VerificaGPS = () => {
+
+    this.diagnostic.isGpsLocationEnabled().then(this.FuncionaGPS, this.ErrorGPS);
+
+  }
+
+  GPSHabilitado() {
+
+    setTimeout(this.VerificaGPS, 5000);
+
+  }
+
+  //Evento de fijado de gps
+  FijarPosicion() {
+
+    this.PosicionFija['latitud'] = this.PosicionActual['latitud'];
+    this.PosicionFija['longitud'] = this.PosicionActual['longitud'];
+    this.distanciaCoord();
+
+  }
+
+  //Calcular distancia
+  distanciaCoord() {
+
+    //double radioTierra = 3958.75;//en millas  
+    var radioTierra: number = 6371;//en kilómetros  
+    var dLat: number = ((this.PosicionActual['latitud'] - this.PosicionFija['latitud']) * Math.PI) / 180;
+    var dLng = ((this.PosicionActual['longitud'] - this.PosicionFija['longitud']) * Math.PI) / 180;
+    var sindLat = Math.sin(dLat / 2);
+    var sindLng = Math.sin(dLng / 2);
+    var va1 = Math.pow(sindLat, 2) + Math.pow(sindLng, 2)
+      * Math.cos(this.PosicionFija['latitud'] * Math.PI / 180) * Math.cos(this.PosicionActual['latitud'] * Math.PI / 180);
+    var va2 = 2 * Math.atan2(Math.sqrt(va1), Math.sqrt(1 - va1));
+    var distancia = radioTierra * va2;
+
+    this.Distancia = distancia.toFixed(4);
+
+    if(this.Distancia<2 && this.Distancia>-2){
+
+      this.Puerta = 'Abierta';
+
+    }
+    else if(this.Puerta == 'Abierta'){
+
+      this.Puerta = 'Cerrada';
 
     }
 
   }
 
-  getGeolocation(){
-
-    //this.geolocation.getCurrentPosition(this.options).then(this.FuncionaGPS).catch(this.ErrorGPS);
-
-    let escuchador = this.geolocation.watchPosition(this.options);
-    escuchador.subscribe(this.FuncionaGPS,this.ErrorGPS);
-
-  }
 
   ngOnInit() {
-    
-    //this.getGeolocation();
-    this.diagnostico.isLocationEnabled().then((activo) => {
 
-      var estado = (activo ? 'ACTIVO' : 'DESACTIVO');
-      console.log("GPS " + estado);
-      this.lblEstadoGPS = estado;
+    //this.GPSHabilitado();
+    this.Localizacion();
 
-    }).catch( (error) => {
-
-      console.log("Ocurrio un error " + error);
-      this.lblEstadoGPS = 'error';
-
-    });
-  
   }
 
 
