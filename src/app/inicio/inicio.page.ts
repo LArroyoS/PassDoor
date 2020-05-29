3ff1988bc5385381e5db8f2121c3da3368950ace
LeonardoA <larroyos1@toluca.tecnm.mx>
2020-05-28 22:47:44 -0500

    Correccion de un detalle con respecto a el calculo de la distancia
    Se olvido colocar un metodo, pero en este comit, ya se implementa, corrigiendo un error respecto e el

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -159,7 +159,7 @@ export class Inicio implements OnInit {
     var R = 6378.137; //Radio de la tierra en km
     var dLat = this.rad( lat2 - lat1 );
     var dLong = this.rad( lon2 - lon1 );
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(this.(lat1)) * Math.cos(rad(lat2)) * Math.sin(dLong/2) * Math.sin(dLong/2);
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(this.rad(lat1)) * Math.cos(this.rad(lat2)) * Math.sin(dLong/2) * Math.sin(dLong/2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
     var d = R * c;
 
